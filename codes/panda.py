6df9be20e4faad7a95a4269e45f0c85de616ebdb
jayson <jksim1833@gmail.com>
2019-11-06 16:59:07 +0900

    pandas test

---
+++ codes/panda.py
@@ -0,0 +1,7 @@
+import pandas as pd
+series_ex= pd.Series([100,90,120,110,105],index =['월','화','수','목','금'])
+#'print series only'
+print(series_ex)
+
+#print series by index'
+print(series_ex['월'])
